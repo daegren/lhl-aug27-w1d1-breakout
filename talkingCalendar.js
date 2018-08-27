f6b48d703ae5177e427c1005d1b692cc94898e46
David Mills <dave@1morepx.com>
2018-08-27 15:59:42 -0400

    refactor parseMonth to it's own function

--- talkingCalendar.js
+++ talkingCalendar.js
@@ -1,3 +1,31 @@
+var parseMonth = function(month) {
+  if (month === 1) {
+    return "January";
+  } else if (month === 2) {
+    return "February";
+  } else if (month === 3) {
+    return "March";
+  } else if (month === 4) {
+    return "April";
+  } else if (month === 5) {
+    return "May";
+  } else if (month === 6) {
+    return "June";
+  } else if (month === 7) {
+    return "July";
+  } else if (month === 8) {
+    return "August";
+  } else if (month === 9) {
+    return "September";
+  } else if (month === 10) {
+    return "October";
+  } else if (month === 11) {
+    return "November";
+  } else if (month === 12) {
+    return "December";
+  }
+};
+
 var talkingCalendar = function(date) {
   // Your code here
   var components = date.split("/");
@@ -8,32 +36,7 @@ var talkingCalendar = function(date) {
 
   console.log(year, month, day);
 
-  var monthString;
-  if (month === 1) {
-    monthString = "January";
-  } else if (month === 2) {
-    monthString = "February";
-  } else if (month === 3) {
-    monthString = "March";
-  } else if (month === 4) {
-    monthString = "April";
-  } else if (month === 5) {
-    monthString = "May";
-  } else if (month === 6) {
-    monthString = "June";
-  } else if (month === 7) {
-    monthString = "July";
-  } else if (month === 8) {
-    monthString = "August";
-  } else if (month === 9) {
-    monthString = "September";
-  } else if (month === 10) {
-    monthString = "October";
-  } else if (month === 11) {
-    monthString = "November";
-  } else if (month === 12) {
-    monthString = "December";
-  }
+  var monthString = parseMonth(month);
 
   console.log(monthString);
 };
