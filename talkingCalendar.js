0e55cd8dd0910de29efd55d38a4218069e529096
David Mills <dave@1morepx.com>
2018-08-27 16:07:47 -0400

    figuring out the suffix for a given date

--- talkingCalendar.js
+++ talkingCalendar.js
@@ -32,13 +32,24 @@ var talkingCalendar = function(date) {
 
   var year = components[0];
   var month = parseInt(components[1], 10);
-  var day = components[2];
+  var day = parseInt(components[2], 10);
 
   console.log(year, month, day);
 
   var monthString = parseMonth(month);
 
   console.log(monthString);
+
+  var daySuffix;
+  if (day === 1 || day === 21 || day === 31) {
+    daySuffix = "st";
+  } else if (day === 2 || day === 22) {
+    daySuffix = "nd";
+  } else if (day === 3 || day === 23) {
+    daySuffix = "rd";
+  } else {
+    daySuffix = "th";
+  }
 };
 
 console.log(talkingCalendar("2017/12/02"));
