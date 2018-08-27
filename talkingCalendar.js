be4d5fc6a44138bcfab9173e12e92d685c80448d
David Mills <dave@1morepx.com>
2018-08-27 15:47:02 -0400

    breaking the date out into variables

--- talkingCalendar.js
+++ talkingCalendar.js
@@ -1,7 +1,14 @@
 var talkingCalendar = function(date) {
   // Your code here
+  var components = date.split("/");
+
+  var year = components[0];
+  var month = components[1];
+  var day = components[2];
+
+  console.log(year, month, day);
 };
 
 console.log(talkingCalendar("2017/12/02"));
-console.log(talkingCalendar("2007/11/11"));
-console.log(talkingCalendar("1987/08/24"));
+// console.log(talkingCalendar("2007/11/11"));
+// console.log(talkingCalendar("1987/08/24"));
